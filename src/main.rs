9b316e01751e47c89bbb4d5ef5979d1e7ae6cd2e
Thomas Brigham <thomas@thomasbrigham.me>
2019-03-09 13:09:21 -0800

    Iron Router implemented

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::env;
-mod input;
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut router = Router::new();           // Alternative syntax:
+    router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
+    router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
+    router.get("/test/:booger", booger_handler, "booger");  //                      query: get "/:query" => handler);
 
-    println!("{:?}", args);
+    Iron::new(router).http("localhost:3000").unwrap();
 
-    let result = input::parse_args(args);
-    println!("result = {:?}", result);
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
+
+    fn booger_handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>().unwrap().find("booger").unwrap_or("/");
+        let booger = format!("the booger is = {:?}", *query);
+        Ok(Response::with((status::Ok, booger)))
+    }
 }
