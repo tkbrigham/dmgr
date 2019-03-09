272e37ef572504b247a1d8dd6fdaa1710ac5b425
Thomas Brigham <thomas@thomasbrigham.me>
2019-03-09 15:28:21 -0800

    registration is working!

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,43 @@ extern crate router;
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use std::io::Read;
+use std::thread;
 
 fn main() {
-    let mut router = Router::new();           // Alternative syntax:
-    router.get("/", handler, "index");        // let router = router!(index: get "/" => handler,
-    router.get("/:query", handler, "query");  //                      query: get "/:query" => handler);
-    router.get("/test/:booger", booger_handler, "booger");  //                      query: get "/:query" => handler);
+    let mut router = Router::new();
+    router.get("/", handler, "index");
+    router.post("/register", register, "register");
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    println!("fffffirst here");
+    let listening = Iron::new(router).http("localhost:3000").unwrap();
+    println!("here now!");
 
     fn handler(req: &mut Request) -> IronResult<Response> {
+        println!("hit main handler");
         let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
         Ok(Response::with((status::Ok, *query)))
     }
 
-    fn booger_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>().unwrap().find("booger").unwrap_or("/");
-        let booger = format!("the booger is = {:?}", *query);
-        Ok(Response::with((status::Ok, booger)))
+    fn register(req: &mut Request) -> IronResult<Response> {
+        println!("hit register handler");
+        let mut s = String::new();
+        let len = req.body.read_to_string(&mut s);
+        println!("{}", s);
+
+
+        thread::spawn(move || {
+            let mut router = Router::new();
+            router.get("/new_endpoint", new_endpoint_handler, "new_endpoint");
+            let listening_pt_2 = Iron::new(router).http("localhost:3001").unwrap();
+
+            fn new_endpoint_handler(req: &mut Request) -> IronResult<Response> {
+                println!("I am inside new endpoint handler");
+                Ok(Response::with((status::Ok, "yes this is new endpoint")))
+            }
+        });
+
+        println!("I should be going next");
+        Ok(Response::with((status::Ok, format!("registered new handler: {:?}", req))))
     }
 }
